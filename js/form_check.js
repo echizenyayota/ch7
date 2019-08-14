e61ea07fc61ee81bf377e0c323e7e17407b977a0
echizenyayota <virtualmisso30@gmail.com>
2019-08-14 21:43:23 +0900

    180 チェックボックスの値を取得したい(その3)

--- js/form_check.js
+++ js/form_check.js
@@ -1,6 +1,8 @@
 let mail = document.getElementById('mail');
 
 mail.addEventListener('change', () => {
+  // 参考URL
+  // https://developer.mozilla.org/ja/docs/Web/HTML/Element/input/checkbox#checked
   if (mail.checked) {
     console.log(mail.value);
   } else {
