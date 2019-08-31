6dd49c1c1d43909e6f75c68065be5be79978994d
echizenyayota <virtualmisso30@gmail.com>
2019-08-31 16:42:00 +0900

    182 複数選択できるリストボックスの値を取得・設定したい

---
+++ js/form_list.js
@@ -0,0 +1,17 @@
+let getListBox = function(name) {
+  let result = [];
+  let elems = document.getElementById(name).options;
+
+  for (let i = 0; i < elems.length; i++) {
+    if (elems[i].selected) {
+      result.push(elems[i].value);
+    }
+  }
+  return result;
+};
+
+const os = document.getElementById('os');
+os.addEventListener('change', () => {
+  console.log(getListBox('os'));
+});
+
